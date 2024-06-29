fc6bf5d8db23b9f9d4309710bc1273883a6e82c6
phuonganh235 <anhptphe170125@fpt.edu.vn>
2024-06-29 22:42:20 +0700

    Models

---
+++ PT10/PT10/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PT10.Models;
+
+public partial class Department
+{
+    public int DepartmentId { get; set; }
+
+    public string? DepartmentName { get; set; }
+
+    public string? DepartmentType { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
