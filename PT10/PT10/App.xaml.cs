52ea6b37818c72522ca74a68b0ac91ebcd62f217
phuonganh235 <anhptphe170125@fpt.edu.vn>
2024-06-29 23:12:59 +0700

    form

--- PT10/PT10/App.xaml.cs
+++ PT10/PT10/App.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PT10.Models;
+using System.Configuration;
 using System.Data;
 using System.Windows;
-
+using System.Linq;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 namespace PT10
 {
     /// <summary>
@@ -9,6 +14,17 @@ namespace PT10
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; set; }
+        public IConfiguration Configuration { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<MainWindow>();
+            serviceCollection.AddScoped<PrnSpr23B1Context>();
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            ServiceProvider.GetRequiredService<MainWindow>().Show();
+        }
     }
 
 }
--- PT10/PT10/MainWindow.xaml.cs
+++ PT10/PT10/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using PT10.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,121 @@ namespace PT10
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly PrnSpr23B1Context _context;
+        public class Courtesy
         {
+            public string Name { get; set; }
+            public string Code { get; set; }
+        }
+        public MainWindow(PrnSpr23B1Context context)
+        {
+
+            _context = context;
             InitializeComponent();
+            courtesy.ItemsSource = new List<Courtesy>()
+            {
+                new Courtesy { Name = "Dr.", Code = "Dr" },
+                new Courtesy { Name = "Mr.", Code = "Mr" },
+                new Courtesy { Name = "Mrs.", Code = "Mrs" },
+                new Courtesy { Name = "Ms.", Code = "Ms" }
+            };
+            department.ItemsSource = _context.Departments.ToList();
+            department.SelectedIndex = 0;
+            UpdateGridView();
+
+        }
+        private Employee GetEmployeeObject()
+        {
+
+            Employee employee = new Employee
+            {
+                EmployeeId = string.IsNullOrEmpty(employeeId.Text) ? 0 : int.Parse(employeeId.Text),
+                FirstName = firstName.Text,
+                LastName = lastName.Text,
+                DepartmentId = ((Department)department.SelectedItem).DepartmentId,
+                Title = title.Text,
+                TitleOfCourtesy = ((Courtesy)courtesy.SelectedItem).Name,
+                BirthDate = dob.SelectedDate
+            };
+            return employee;
+        }
+
+
+        private void UpdateGridView()
+        {
+            lvEmployee.ItemsSource = null;
+            lvEmployee.ItemsSource = _context.Employees.Include(x => x.Department).ToList();
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Employee employee = GetEmployeeObject();
+                employee.EmployeeId = 0;
+                _context.Employees.Add(employee);
+                if (_context.SaveChanges() > 0)
+                {
+                    MessageBox.Show("Add Employee Success");
+                    UpdateGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter correct information");
+            }
+        }
+
+        private void Button_Refresh(object sender, RoutedEventArgs e)
+        {
+            employeeId.Text = string.Empty;
+            firstName.Text = string.Empty;
+            lastName.Text = string.Empty;
+            department.SelectedIndex = 0;
+            title.Text = string.Empty;
+            courtesy.SelectedIndex = 0;
+            dob.SelectedDate = null;
+        }
+        private void Button_Edit(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Employee temployee = GetEmployeeObject();
+                var employ = _context.Employees.FirstOrDefault(x => x.EmployeeId == temployee.EmployeeId);
+                if (employ != null)
+                {
+                    employ.FirstName = temployee.FirstName;
+                    employ.LastName = temployee.LastName;
+                    employ.DepartmentId = temployee.DepartmentId;
+                    employ.TitleOfCourtesy = temployee.TitleOfCourtesy;
+                    employ.Title = temployee.Title;
+                    employ.BirthDate = temployee.BirthDate;
+                    _context.Employees.Update(employ);
+                    if (_context.SaveChanges() > 0)
+                    {
+                        MessageBox.Show("Edit Employee Success");
+                        UpdateGridView();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please enter correct information");
+            }
+        }
+
+        private void btnClickOnGrid(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+            if (item != null)
+            {
+                var employee = ((Employee)item);
+                if (employee != null)
+                {
+
+                }
+            }
+
         }
     }
 }
